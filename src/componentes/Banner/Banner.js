bc2ca053832586eed054fa8e5710d0b03b6c7595
naylahonoratos <naylahonorato@gmail.com>
2023-05-07 00:02:38 -0300

    Projeto Organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+ import './Banner.css'
+ function Banner(){
+    return (
+        <header className="banner">
+            <img src="./imagens/imagens/banner.png" alt="O banner principal da pagina"/>
+        </header>
+    )
+
+ }
+
+
+ export default Banner
